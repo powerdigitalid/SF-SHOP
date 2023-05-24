c2a10df5c8b9ec02c829124118024908232c76ca
dev@powerdigital.id <raflyputra0510@gmail.com>
2023-05-24 23:38:22 +0700

    fix admin

--- components/admin-pages/formpemesanan/tabelpemesanan.js
+++ components/admin-pages/formpemesanan/tabelpemesanan.js
@@ -20,6 +20,8 @@ export default function Tabelpemesanan() {
                                             <tr>
                                                 <th scope="col">No.</th>
                                                 <th scope="col">Email</th>
+                                                <th scope="col">Alamat</th>
+                                                <th scope="col">No.Hp</th>
                                                 <th scope="col">Total</th>
                                                 <th scope="col">Ekspedisi</th>
                                                 <th scope="col">Actions</th>
@@ -29,12 +31,14 @@ export default function Tabelpemesanan() {
                                             <tr>
                                                 <th scope="row">1</th>
                                                 <td>Exampel@gmail.com</td>
+                                                <td>Jl. Hasyim Asyhari, No.20, Jakarta</td>
+                                                <td>0226423974</td>
                                                 <td>Rp. 350.000</td>
                                                 <td>JNE</td>
                                                 <td>
-                                                    <Link href="/admin/pemesanan/detail" className="btn btn-primary mr-1 rounded text-white"><i className="far fa-eye mr-1" />Detail</Link>
-                                                    <button className="btn btn-success mr-1 rounded text-white"><i className="fas fa-edit mr-1" />Confirmasi</button>
-                                                    <button className="btn btn-danger rounded text-white"><i className="far fa-times-circle mr-1" />Tolak</button>
+                                                    <Link href="/admin/pemesanan/detail" className="btn btn-primary mr-1 rounded mb-1 text-white"><i className="far fa-eye" /></Link>
+                                                    <button className="btn btn-success mr-1 rounded mb-1 text-white"><i className="far fa-edit" /></button>
+                                                    <button className="btn btn-danger rounded mb-1 text-white"><i className="far fa-times-circle" /></button>
                                                 </td>
                                             </tr>
                                         </tbody>
